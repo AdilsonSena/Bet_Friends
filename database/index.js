b78ae73ddd08407aac1f48493d69e7a249d00523
Nicholas Ugadin Borsato <nicoborsato@gmail.com>
2022-05-16 19:03:47 -0300

    Update index.js
    Arrumando conflitos

--- database/index.js
+++ database/index.js
@@ -1,30 +1,19 @@
-
 const Sequelize = require('sequelize');
 const configDB = require('./config/config.js');
+const connection = new Sequelize(configDB)
+
 const User = require('./models/user');
 const User_autorizado = require('./models/user_autorizado');
 const Autorizacao = require('./models/autorizacao');
 const ApostaHasJogo = require('./models/apostaHasJogo');
-const connection = new Sequelize(configDB)
+const Campeonato = require("./models/campeonato");
+const Data = require("./models/data");
 
 User.init(connection);
 User_autorizado.init(connection);
 Autorizacao.init(connection);
 ApostaHasJogo.init(connection);
-
-module.exports = connection;
-
-
-const Sequelize = require("sequelize");
-const configDB = require("./config/config.js");
-const User = require("./models/user");
-const Campeonato = require("./models/campeonato");
-const Data = require("./models/data");
-const connection = new Sequelize(configDB);
-
-User.init(connection);
 Campeonato.init(connection);
 Data.init(connection);
 
 module.exports = connection;
-
