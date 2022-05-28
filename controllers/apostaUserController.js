9238ca1662933a7f3b671c2ae15378b143b56243
Andersonvin <andersonvin.eng@outlook.com>
2022-05-27 22:12:35 -0300

    pq sim

--- controllers/apostaUserController.js
+++ controllers/apostaUserController.js
@@ -3,12 +3,12 @@ const apostasUserModel = require('../database/models/apostaUser');
 
 const apostasUserController = {
     createApostaUser: async (req, res) => {
-        const { apostas_id, user_id } = req.body;
+        const { apostas_id, user_id, side } = req.body;
 
         try {
             const apostaUser = await apostasUserModel.create({
                 apostas_id,
-                user_id
+                user_id, side
             });
 
             res.status(201).json(apostaUser);
@@ -62,7 +62,22 @@ const apostasUserController = {
             res.status(400).json(error);
             return;
         }
+    },
+    findUserApostas: async (req, res) => {
+        const { id } = req.params;
+
+        try {
+            const apostaUser = await apostasUserModel.findAll({
+                where: { user_id: id }
+            });
+
+            res.status(200).json(apostaUser);
+            return;
+        } catch (error) {
+            res.status(400).json(error);
+            return;
+        }
     }
-}
+};
 
 module.exports = apostasUserController;
